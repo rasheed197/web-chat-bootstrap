e06df71a1a3348fe2c2d8a730ede01af884d7427
rasheed197 <94674581+rasheed197@users.noreply.github.com>
2026-05-03 15:42:31 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const toggle = document.querySelector('.floating-chat')
+
+const mainLeft = document.querySelector('.left-section')
+const mainRight = document.querySelector('.right-section')
+
+toggle.addEventListener('click', () => {
+    console.log("Toggle")
+    if (mainLeft.classList.contains('hide')) {
+        mainLeft.classList.remove('hide')
+    } else  {
+        mainLeft.classList.add('hide')
+    }
+
+    if (mainRight.classList.contains('show')) {
+        mainRight.classList.remove('show')
+    } else {
+        mainRight.classList.add('show')
+    }
+})
